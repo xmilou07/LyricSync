24b3ba8b5f85314cf6e45befa7a56b4a26d6d0a1
x.milou07 <ps266424@summacollege.nl>
2025-10-26 12:59:25 +0100

    all pages follow home styling

---
+++ LyricSync/Areas/Identity/Data/LyricSyncContext.cs
@@ -0,0 +1,22 @@
+﻿using LyricSync.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LyricSync.Data;
+
+public class LyricSyncContext : IdentityDbContext<ApplicationUser>
+{
+    public LyricSyncContext(DbContextOptions<LyricSyncContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
